9c5c6159c8f6cdda4d0ecd95f27c45908bbaf5ff
Devon <ddarrowphl@gmail.com>
2022-10-02 18:33:56 -0400

    customized carousel buttons

--- app.js
+++ app.js
@@ -1,23 +1,23 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
-const path = require('path');
+const path = require("path");
 
 // Run nodemon server/app.js from terminal to develop locally
 
-app.set('views', path.join(__dirname, 'views')); 
-app.set('view engine', 'ejs');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
 
 app.listen(port, () => {
-    console.log(`Server is running on port ${port}`)
+  console.log(`Server is running on port ${port}`);
 });
 
 // Middleware & static files
-app.use(express.static('public/styles'));
-app.use(express.static('public/assets'));
+app.use(express.static("public/styles"));
+app.use(express.static("public/assets"));
 
-app.get('/', (req, res) => {
-  res.render('index');
+app.get("/", (req, res) => {
+  res.render("index");
 });
 
 module.exports = app;
